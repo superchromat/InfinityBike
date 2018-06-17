e17491390bab5452f64bafd856d1da2e1575ec04
Alexandre Doucet <alexandre_doucet4@hotmail.com>
2018-06-17 14:42:22 -0300

    Fixed the main screen softlock if you entered the options menu.

---
+++ Infinity Bike/Assets/UI/MenuButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public enum Menu
+    {
+        STARTSCREEN = 0, OPTIONSCEEN = 1
+    }
+    public List<MenueObjects> optionButtons = new List<MenueObjects>();
+
+    public void ToggleMenuButtons(int id)
+    {
+        foreach (GameObject item in optionButtons[id].menuObject)
+        {
+            item.SetActive(!item.activeSelf);
+        }   
+    }
+
+    
+    public void ExitGame()
+    {
+
+        Debug.Log(GameObject.FindObjectsOfType(typeof(ArduinoThread)).Length);
+
+        try
+        {
+            Application.Quit();
+        }
+        catch
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+    }
+
+    public void LoadScene(int scene)
+    {
+
+        SceneManager.LoadScene(scene);
+    }
+
+    [Serializable]
+    public class MenueObjects
+    {
+        public string menuName;
+        public List<GameObject> menuObject = new List<GameObject>();   
+
+    }
+
+}
