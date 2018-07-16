f07f6f7dbbcf3a0603f4e87f7d522c8baa511085
Alexandre Doucet <alexandre_doucet4@hotmail.com>
2018-07-15 21:14:30 -0300

    Added a respawn option to the menu in the main game.

--- Infinity Bike/Assets/Player/Respawn/Respawn.cs
+++ Infinity Bike/Assets/Player/Respawn/Respawn.cs
@@ -5,17 +5,17 @@ using System;
 
 public class Respawn : MonoBehaviour {
 
-	public TrackNode trackNode;
+	public TrackNode trackNode = null;
 	public float verticalRespawnPoint = 50f;
-	private Rigidbody rb;
-    public Action onRespawn;
-    int minDistanceNode;
+	private Rigidbody rb = null;
+    public Action onRespawn = null;
+    int minDistanceNode = 0;
 
     void Start () 
 	{	
 		rb = GetComponent<Rigidbody> ();
-        onRespawn = RespawnObject;
-        onRespawn();
+        onRespawn += RespawnObject;
+        CallOnRespawn();
     }
     
     void LateUpdate () 
@@ -23,41 +23,41 @@ public class Respawn : MonoBehaviour {
         minDistanceNode = FindNearestNode(trackNode, transform);
         if (  Vector3.Distance( trackNode.GetNode(minDistanceNode),transform.position) > verticalRespawnPoint)
 		{
-            onRespawn();
-        }
+            CallOnRespawn();
+        }   
 	}
+    public void CallOnRespawn()
+    {onRespawn();}
 
-	private void RespawnObject()
-	{   
-		if (trackNode.GetNodeCount () <= 1) 
-		{return;}
 
-		if (minDistanceNode >= trackNode.GetNodeCount()) 
-		{minDistanceNode = 0;}
 
-		if (rb != null) 
-		{	
-			rb.velocity = Vector3.zero;
-			rb.angularVelocity = Vector3.zero;
-		}	
+    private void RespawnObject()
+    {
+        if (trackNode.GetNodeCount() <= 1)
+        { return; }
 
-		WheelCollider[] wheel = GetComponentsInChildren<WheelCollider> ();
-		foreach (WheelCollider item in wheel) 
-		{   
+        if (minDistanceNode >= trackNode.GetNodeCount())
+        { minDistanceNode = 0; }
+
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        WheelCollider[] wheel = GetComponentsInChildren<WheelCollider>();
+        foreach (WheelCollider item in wheel)
+        {
             item.brakeTorque = 1000f;
             item.motorTorque = 0;
             item.steerAngle = 0;
 
-        }   
+        }
 
-		transform.position = trackNode.GetNode(minDistanceNode);
-		transform.forward = trackNode.GetNode(minDistanceNode + 1) - transform.position;
+        transform.position = trackNode.GetNode(minDistanceNode);
+        transform.forward = trackNode.GetNode(minDistanceNode + 1) - transform.position;
 
-	}	
-
-
-
-
+    }
 
 	static public int FindNearestNode(TrackNode respawnPoint, Transform objToRespawn)
 	{
