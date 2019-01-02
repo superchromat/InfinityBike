c0cc984363cd731a2c3ea6662105cead3f775ec6
MxBoud <emb4733@gmail.com>
2019-01-01 20:52:51 -0400

    In the process of implementing the 3d model in the main scene.

---
+++ Infinity Bike/Assets/PlayerAnimatorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorScript : MonoBehaviour {
+    private Animator playerAnimator;
+
+   //[SerializeField]
+    private float steerAngle; 
+    public float SteerAngle
+    {
+        get
+        {
+            return steerAngle;
+        }
+        set
+        {
+            steerAngle = value;
+            Debug.Log("Set");
+            UpdateSteerAngle();
+        }
+    }
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        playerAnimator = GetComponent<Animator>();
+
+       
+
+    }
+	
+	// Update is called once per frame
+	void UpdateSteerAngle () {
+        playerAnimator.Play("Steering", 1, steerAngle);
+	}
+}
